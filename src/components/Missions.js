0dc86843fc68081ddeda29c2293cfcfc1f5770e8
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-23 12:01:55 +0200

    Install react-bootstrap, add basic structure of mission page

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,44 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+
 function Missions() {
   return (
-    <div>
-      <h1>Missionas</h1>
-    </div>
+    <Table striped bordered hover size="lg">
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Thaicom</td>
+          <td>Otto</td>
+          <div className="col-md-4">
+            <tr>
+              <Button variant="primary">NOT A MEMBER</Button>
+              {' '}
+              <Button variant="outline-secondary">Join Mission</Button>
+              {' '}
+            </tr>
+          </div>
+        </tr>
+        <tr>
+          <td>Telstar</td>
+          <td>Otto</td>
+          <div className="col-md-4">
+            <tr>
+              <Button variant="primary">NOT A MEMBER</Button>
+              {' '}
+              <Button variant="outline-secondary">Join Mission</Button>
+              {' '}
+            </tr>
+          </div>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
