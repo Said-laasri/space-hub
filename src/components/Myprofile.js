c47ee9c1c8207d0587894c00d79428108c2163c9
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-08-24 12:44:48 +0100

    My profile reserved mission and rockets

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,7 +1,25 @@
+import { useSelector } from 'react-redux';
+
 function Myprofile() {
+  const profileParts = useSelector((state) => [
+    {
+      RocketTitle: 'My Missions',
+      data: state.missions.filter((mission) => mission.reserved),
+    },
+    {
+      RocketTitle: 'My Rockets',
+      data: state.rockets.filter((rocket) => rocket.reserved),
+    },
+  ]);
+
   return (
     <div>
-      <h1>myprofile</h1>
+      <div className="misssion-joined">
+        <h4></h4>
+      </div>
+      <div className="rocket-reserved">
+        <h4></h4>
+      </div>
     </div>
   );
 }
